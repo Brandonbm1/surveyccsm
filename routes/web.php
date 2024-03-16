994e7be887c0bf4f2caf5920a458411c9b6b4738
Brandon <brandon.uan@gmail.com>
2024-03-16 02:25:18 -0500

    Update web routes for authentication and login and setting middlewares

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\Login;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,13 +17,20 @@ use App\Http\Controllers\QuestionController;
 |
 */
 
-
-Route::get('/', [QuestionController::class, 'getActiveQuestions']);
-Route::get('edit', function () {
-    return view('pages.edit');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [QuestionController::class, 'getActiveQuestions']);
+    Route::get('edit', [QuestionController::class, 'editView'])->middleware('isAdmin');
+    Route::post('create_question', [QuestionController::class, 'create'])->name('create_question');
+    Route::post('send_answer', [AnswerController::class, 'reply'])->name('send_answer');
+    Route::get('stats', [QuestionController::class, 'index'])->name('stats');
+    Route::get('answers/{id}/{type}', [AnswerController::class, 'index'])->name('answers');
+    Route::put('update_question', [QuestionController::class, 'update'])->name('update_question');
+    Route::get('/logout', [Login::class, 'logout'])->name('logout');
 });
-Route::post('create_question', [QuestionController::class, 'create'])->name('create_question');
-Route::post('send_answer', [AnswerController::class, 'reply'])->name('send_answer');
-Route::get('stats', [QuestionController::class, 'index'])->name('stats');
-Route::get('answers/{id}/{type}', [AnswerController::class, 'index'])->name('answers');
-Route::put('update_question', [QuestionController::class, 'update'])->name('update_question');
+
+Route::get('/login', [Login::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [Login::class, 'store'])->name('login')->middleware('guest');
+
+Route::get('/{any}', function () {
+    return redirect('/');
+})->where('any', '.*');
